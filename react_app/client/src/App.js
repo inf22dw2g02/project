31cc69b0e8e71eaf04880fff052f5816f8d25852
Brunobk2c <bk2c@hotmail.com>
2023-06-06 19:14:28 +0100

    UPDATE APP.JS

--- react_app/client/src/App.js
+++ react_app/client/src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import LoginButton from "./components/login";
-import LogoutButton from "./components/logout";
+import  LoginButton from "./components/login";
+import  LogoutButton from "./components/logout";
+import HomePage from './components/HomePage';
 import { useEffect } from 'react';
-import { gapi } from 'gapi-script';
-import React from 'react';
+import { gapi}  from  'gapi-script';
 import Navbar from './components/Navbar';
-
 
 const clientId = '89846874244-4rl59hshpt44k64ontkslptsit9jmilq.apps.googleusercontent.com';
 
-const App = () => {
+function App() {
 
-    useEffect(() => {
+  useEffect(() => {
     function start() {
       gapi.client.init({
         clientId: clientId,
@@ -23,17 +22,18 @@ const App = () => {
   });
 
 
+  // var accessToken = gapi.auth.getToken().accessToken; ### Para ter o acces token para a api
+  
+
+
   return (
-    <div className='App'>
+    <div className="App">
       <Navbar />
+      <HomePage />
       <LoginButton />
-      <LogoutButton />      
+      <LogoutButton />
     </div>
   );
 }
-
-  // var accessToken = gapi.auth.getToken().accessToken; ### Para ter o acces token para a api
-
-
 
 export default App;
---
+++ react_app/client/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Home Page!</h1>
+      <p>You are logged in and can access the content of this page.</p>
+    </div>
+  );
+};
+
+export default HomePage;
