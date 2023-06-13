b6b1e7bed05fd1371b86b7da0c4f544dad34da1a
Brunobk2c <bk2c@hotmail.com>
2023-06-13 21:56:01 +0100

    Creation of a bokk Highlight on home page

--- react_app/client/src/components/HomePage.js
+++ react_app/client/src/components/HomePage.js
@@ -1,6 +1,8 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
 import './home.css';
+import BookHighlightSection from './BookHighlightSection';
+import { Link } from 'react-router-dom';
+
 
 
 const HomePage = () => {
@@ -8,6 +10,10 @@ const HomePage = () => {
     <div className="container">
       <h1 className="title">Welcome to the Home Page!</h1>
       <p className="description">You are logged in and can access the content of this page.</p>
+      <h2>Destaques</h2>
+      <BookHighlightSection />
+      <h2>Clica no botao abaixo para para aceder a Livros </h2>
+      <Link to="/book" className="button">Livros</Link>
     </div>
   );
 };
